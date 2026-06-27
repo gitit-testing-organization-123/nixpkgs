6455ae799c9a32dc9debf0a4309ee29de4c3546a
Conor E. Olive <conor@conorolive.com>
2026-06-27 08:19:45 -0700

    bump

--- pkgs/gitit-basilisk/default.nix
+++ pkgs/gitit-basilisk/default.nix
@@ -8,8 +8,8 @@ let
   gititSrc = fetchFromGitHub {
     owner = "gitit-testing-organization-123";
     repo = "gitit";
-    rev = "38e8b18f7aa260eedbb77352b2c680d1c0b469cf";
-    sha256 = "18mwn1gzjmi1w8lm0hi3rz06459an3p7zwl81vs5lhqjy5d7zki3";
+    rev = "a3468655bccbb3d398e7269b993b3ec52b5b4e6d";
+    sha256 = "1zvypfifn5bi7d5n5w1gqp5mpkifrcnb5czq2zjy6ibpjp8b2xwq";
   };
 
   gitit = haskellPackages.callCabal2nix "gitit" gititSrc { };
